226e8027f911c53a5f88a5e80dc3aa44eb64cc89
marykasp <kasparian.mary@gmail.com>
2023-09-11 09:44:50 -0500

    feat: add routes to home and product details page

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,19 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
-  return <div className="overflow-hidden"></div>;
+  return (
+    <div className="overflow-hidden">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+        </Routes>
+        <Sidebar />
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
