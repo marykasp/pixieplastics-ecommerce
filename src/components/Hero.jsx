bd8d8569afe313e8eaa41a1501f397e4bf9c2937
marykasp <kasparian.mary@gmail.com>
2023-09-11 14:55:47 -0500

    style: add hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,33 @@
+import HeroImg from '../assets/vinyl-toystore.png';
+import { Link } from 'react-router-dom';
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="bg-primary h-[700px] py-24 mt-6">
+      <div className="container mx-auto flex justify-around h-full">
+        <div className="flex flex-col justify-center">
+          <div className="font-semibold flex items-center uppercase">
+            <div className="w-10 h-[2px] bg-secondary mr-2"></div> New Toys
+          </div>
+          <h1 className="text-[70px] leading-[1.1] font-light mb-4">
+            RARE COLLECTIONS <br />
+            <span className="font-semibold">VINYL TOYS</span>
+          </h1>
+          <Link
+            to={'/'}
+            className="self-start text-white uppercase font-semibold flex justify-center items-center py-[11px] px-[21px]
+             bg-secondary rounded-sm hover:bg-tertiary transition-all duration-300"
+          >
+            Discover More
+          </Link>
+        </div>
+        {/* image */}
+        <div className="hidden lg:block">
+          <img src={HeroImg} alt="hero vinyl toy store" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,21 +2,25 @@ import { useContext } from 'react';
 // import Product context
 import { ProductContext } from '../contexts/ProductContext';
 import Product from '../components/Product';
+import Hero from '../components/Hero';
 
 const Home = () => {
   // get products from product context
   const { products } = useContext(ProductContext);
 
   return (
-    <section className="py-16">
-      <div className="container mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-[30px] max-w-sm md:max-w-none mx-auto md:mx-0">
-          {products.map((product) => {
-            return <Product key={product.id} product={product} />;
-          })}
+    <div>
+      <Hero />
+      <section className="py-16">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-[30px] max-w-sm md:max-w-none mx-auto md:mx-0">
+            {products.map((product) => {
+              return <Product key={product.id} product={product} />;
+            })}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
