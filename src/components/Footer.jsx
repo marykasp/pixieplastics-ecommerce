39221ef717a1f3930eaebbc647b02206f2e2c342
marykasp <kasparian.mary@gmail.com>
2023-09-11 15:12:02 -0500

    style: add footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,13 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-primary py-12">
+      <div className="container mx-auto">
+        <p className="text-center text-text">
+          Copyright &copy; Ecommerce Shop 2023. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
