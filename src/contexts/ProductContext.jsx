c575fff2eefaaa078695ea64399095efac854960
marykasp <kasparian.mary@gmail.com>
2023-09-11 10:32:46 -0500

    chore: create fake data for vinyl toy store as a json object
    Fetch data using async/await and save to product context

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -1,5 +1,28 @@
-const ProductContext = () => {
-  return <div>ProductContext</div>;
+import React, { createContext, useState, useEffect } from 'react';
+
+// create context
+const ProductContext = createContext();
+
+const ProductProvider = ({ children }) => {
+  // products state
+  const [products, setProducts] = useState([]);
+
+  // fetch products
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('data.json');
+      const data = await response.json();
+      console.log(data);
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <ProductContext.Provider value={{ products }}>
+      {children}
+    </ProductContext.Provider>
+  );
 };
 
-export default ProductContext;
+export default ProductProvider;
